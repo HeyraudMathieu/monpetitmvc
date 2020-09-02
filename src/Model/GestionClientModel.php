94ab109f5bc3b3c077505cefef52e50aebe0dfd8
Heyraud Mathieu <matheyraud@gmail.com>
2020-09-02 13:08:22 +0200

    create : GestionClientModel

--- src/Model/GestionClientModel.php
+++ src/Model/GestionClientModel.php
@@ -1,6 +1,19 @@
 <?php
 namespace APP\Model;
 
+use \PDO;
+use APP\Entity\Client;
+use Tools\Connexion;
+
 class GestionClientModel {
+    
+    public function find($id) {
+        $unObjetPdo = Connexion::getConnexion();
+        $sql = "select * from client where id = :id";
+        $ligne = $unObjetPdo->prepare($sql);
+        $ligne->bindValue(':id', $id, PDO::PARAM_INT);
+        $ligne->execute();
+        return $ligne->fetchObject(Client::class);
+    }
     
 }
